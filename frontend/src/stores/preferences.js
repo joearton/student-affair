bb3969a274b2d73a711f17f76c2f108e29faffbe
joearton <joearton.id@gmail.com>
2024-12-18 22:59:28 +0000

    Update

--- frontend/src/stores/preferences.js
+++ frontend/src/stores/preferences.js
@@ -3,5 +3,7 @@ import { writable } from 'svelte/store';
 // Inisialisasi store untuk preferences
 export const preferences = writable({
     site_title: 'Site Name',
-    slideshows: [],
+    slideshows: [
+        { image: 'placeholder1.jpg', title: 'Default Slide 1'},
+    ],
 })
