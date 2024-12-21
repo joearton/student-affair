fa6a1db3db9fbd068c1df02ee6931db4928b10d0
Ilham Pangestu <140824958+MinervaMob@users.noreply.github.com>
2024-12-21 16:38:45 +0700

    menyesuaikan tampilan detail post, menambah komponent Side Menu

--- frontend/src/routes/(site)/+page.svelte
+++ frontend/src/routes/(site)/+page.svelte
@@ -1,44 +1,57 @@
-<script lang='ts'>
-    import { preference } from '$lib/stores/preference';
-    import { onMount } from 'svelte';
-    import { getPosts } from '$lib/blog';
+<script lang="ts">
+    import { preference } from "$lib/stores/preference";
+    import { onMount } from "svelte";
+    import { getPosts } from "$lib/blog";
 
-    let posts = $state([{
-        title           : '',
-        content          : '',
-        slug             : '',
-        featured_image   : '',
-        publication_date : '',
-        post_excerpt     : ''
-    }]);
+    let posts = $state([
+        {
+            title: "",
+            content: "",
+            slug: "",
+            featured_image: "",
+            publication_date: "",
+            post_excerpt: "",
+        },
+    ]);
 
     onMount(async () => {
         try {
             posts = await getPosts();
         } catch (error) {
         } finally {
-        }        
-    })
-
+        }
+    });
 </script>
 
-
-<div class=''>
-
+<div class="">
     <!-- Caraousel -->
     <div id="front-carousel" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             {#each $preference?.slideshows as slideshow, index}
                 <div class="carousel-item {index === 0 ? 'active' : ''}">
-                    <img src={slideshow.image} class="d-block w-100" alt={slideshow.title}>
+                    <img
+                        src={slideshow.image}
+                        class="d-block w-100"
+                        alt={slideshow.title}
+                    />
                 </div>
             {/each}
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#front-carousel" data-bs-slide="prev">
+        <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#front-carousel"
+            data-bs-slide="prev"
+        >
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#front-carousel" data-bs-slide="next">
+        <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#front-carousel"
+            data-bs-slide="next"
+        >
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
@@ -47,24 +60,33 @@
     <!-- Mahasiswa Baru -->
     <div class="new-student-section py-5 position-relative">
         <div class="container text-center py-5">
-            <h2 class="fw-bold display-5 text-primary mb-1">Pendaftaran Mahasiswa Baru</h2>
-            <p class="lead mb-4">Bergabunglah bersama kami untuk masa depan lebih cerah.</p>
-            <a href="https://pmb.umko.ac.id" class="btn btn-primary btn-lg px-5 py-2 shadow-lg rounded-pill">
+            <h2 class="fw-bold display-5 text-primary mb-1">
+                Pendaftaran Mahasiswa Baru
+            </h2>
+            <p class="lead mb-4">
+                Bergabunglah bersama kami untuk masa depan lebih cerah.
+            </p>
+            <a
+                href="https://pmb.umko.ac.id"
+                class="btn btn-primary btn-lg px-5 py-2 shadow-lg rounded-pill"
+            >
                 <i class="fa fa-link"></i> Daftar Sekarang
             </a>
         </div>
     </div>
-  
+
     <!-- Features Section -->
     <div class="features-section py-5 bg-primary">
         <div class="container text-center">
             <h2 class="fw-bold mb-2 text-warning">Mengapa Memilih Kami?</h2>
-            <p class="mb-5 text-white">Kami memberikan yang terbaik untuk memastikan kesuksesan Anda.</p>
+            <p class="mb-5 text-white">
+                Kami memberikan yang terbaik untuk memastikan kesuksesan Anda.
+            </p>
             <div class="row gy-4">
                 <div class="col-md-3">
                     <div class="feature-card py-4 px-3 bg-white shadow rounded">
                         <div class="icon-container mb-3">
-                            <i class='fa fa-university'></i>
+                            <i class="fa fa-university"></i>
                         </div>
                         <h3 class="fw-bold">+11</h3>
                         <p>Pilihan Program Studi</p>
@@ -100,26 +122,39 @@
             </div>
         </div>
     </div>
-  
+
     <div class="post-section py-5">
         <div class="container">
-            <h2 class="fw-bold text-center mb-4">Latest Posts</h2>    
+            <h2 class="fw-bold text-center mb-4">Latest Posts</h2>
             {#if posts.length === 0}
-                <p class="text-muted text-center">No posts available at the moment.</p>
+                <p class="text-muted text-center">
+                    No posts available at the moment.
+                </p>
             {:else}
                 <div class="row row-cols-1 row-cols-md-3 g-4">
                     {#each posts.slice(0, 6) as post}
                         <div class="col">
                             <div class="card post-item h-100 shadow-lg">
-                                <a class="card-title mb-1" href={`/post/${post.slug}`} aria-label="{post.title}">
-                                    <div class="post-thumbnail" style="background: url({post.featured_image}) center center no-repeat; background-size: cover;"></div>
+                                <a
+                                    class="card-title mb-1"
+                                    href={`/post/${post.slug}`}
+                                    aria-label={post.title}
+                                >
+                                    <div
+                                        class="post-thumbnail"
+                                        style="background: url({post.featured_image}) center center no-repeat; background-size: cover;"
+                                    ></div>
                                 </a>
                                 <div class="card-body px-4">
-                                    <div class='py-2 text-sm'>
-                                        <i class='fa fa-calendar-alt'></i> {post.publication_date}
+                                    <div class="py-2 text-sm">
+                                        <i class="fa fa-calendar-alt"></i>
+                                        {post.publication_date}
                                     </div>
-                                    <a class="card-title" href={`/post/${post.slug}`}>
-                                        <h5 class='fw-bold'>{post.title}</h5>
+                                    <a
+                                        class="card-title"
+                                        href={`/post/${post.slug}`}
+                                    >
+                                        <h5 class="fw-bold">{post.title}</h5>
                                     </a>
                                     <p class="card-text text-muted py-3">
                                         {post.post_excerpt}
@@ -132,9 +167,7 @@
             {/if}
         </div>
     </div>
-
 </div>
-
 
 <style>
     .text-sm {
@@ -159,13 +192,30 @@
         transition: all 0.5s ease-in-out;
     }
 
+
+    .card:hover {
+        transition: all 0.4s ease;
+        transform: scale(1.02);
+        box-shadow: 2px 2px 8px rgb(0, 0, 0, 0.3);
+    }
+
+    .card-title:hover {
+        transition: all 0.4s ease;
+        color: #002e5c;
+    }
+
     .icon-container {
         display: inline-flex;
         justify-content: center;
         align-items: center;
         width: 90px;
         height: 90px;
-        background-color: rgba(0, 123, 255, 0.1); /* Warna background transparan */
+        background-color: rgba(
+            0,
+            123,
+            255,
+            0.1
+        ); /* Warna background transparan */
         border-radius: 50%;
         margin: 0 auto;
         font-size: 25px;
@@ -175,7 +225,4 @@
         position: relative;
         height: 215px;
     }
-
-
-
 </style>
--- frontend/src/routes/(site)/post/[slug]/+page.svelte
+++ frontend/src/routes/(site)/post/[slug]/+page.svelte
@@ -1,62 +1,133 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import { getPost } from '$lib/blog';
-    import { page } from '$app/state';
+    import { onMount } from "svelte";
+    import { getPost } from "$lib/blog";
+    import { page } from "$app/state";
+    import SidePostMenu from "$lib/components/SidePostMenu.svelte";
 
     let slug = page.params.slug;
     let post = $state({
-        featured_image: '',
-        title: '',
-        subtitle: '',
-        post_excerpt: '',
+        featured_image: "",
+        title: "",
+        status: "",
+        slug: "",
+        subtitle: "",
+        post_excerpt: "",
         author: {
-            profile_picture_url: '',
-            username: ''
+            profile_picture_url: "",
+            username: "",
         },
-        publication_date: '',
-        content: ''
+        publication_date: "",
+        content: "",
     });
- 
+
     onMount(async () => {
-        post = await getPost({slug: slug})
+        post = await getPost({ slug: slug });
     });
 </script>
 
+<svelte:head>
+    <title>{post.title}</title>
+</svelte:head>
 
-<div class="mb-3 post-thumbnail" style="background: url({post.featured_image}) bottom center no-repeat; background-size: cover;">
+<!-- Thumbnail -->
+<div
+    class="mb-3 post-thumbnail"
+    style="background: url({post.featured_image}) bottom center no-repeat; background-size: cover;"
+>
     <div class="overlay"></div>
     <div class="container px-3">
         <div class="position-absolute post-thumbnail-label">
-            <h1 class="h4 bg-primary post-title fw-bold text-warning mb-0 p-3">{post.title}</h1>
+            <h1 class="h4 bg-primary post-title fw-bold text-warning mb-0 p-3">
+                {post.title}
+            </h1>
             {#if post.subtitle}
-                <p class="post-subtitle bg-warning post-subtitle p-3 m-0">{post.post_excerpt}</p>
+                <p class="post-subtitle bg-warning post-subtitle p-3 m-0">
+                    {post.post_excerpt}
+                </p>
             {/if}
         </div>
     </div>
 </div>
 
+<!-- Profil Penulis -->
+<div class="container mt-5">
 
-<div class="container">
+    <div class="info bg-body-secondary rounded-2 p-4 mb-5">
+        <p>Date Publication: {post.publication_date}</p>
+        <div class="btn status bg-success rounded-5 py-1 px-3 text-white mb-2">{post.status}</div>
+        <div class="other-info d-flex justify-content-between text-start mt-3">
+            <div><strong>Level:</strong> <br> Bachelor</div>
+            <div><strong>Source:</strong> <br> Internal Scholarship</div>
+            <div><strong>Goal:</strong> <br> UMKO</div>
+            <div><strong>Awarde:</strong> <br> New Students</div>
+        </div>
+    </div>
+
     <div class="row">
         <div class="col-md-3">
-            <div class="border shadow-sm">
+            <div class="border shadow-sm rounded-5 mb-4">
                 <div class="text-center">
-                    <h5 class='text-center mb-3 p-3 border-bottom'>Tentang Penulis</h5>
-                    <img src={post.author?.profile_picture_url || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ68D1zB62HiAWZAkQpessCgGpmfvJQUX8Rhg&s'} alt={post.author?.username} class="rounded-circle" width="100" height="100" />
+                    <h5 class="text-center mb-3 p-3 border-bottom">
+                        Tentang Penulis
+                    </h5>
+                    <img
+                        src={post.author?.profile_picture_url ||
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ68D1zB62HiAWZAkQpessCgGpmfvJQUX8Rhg&s"}
+                        alt={post.author?.username}
+                        class="rounded-circle"
+                        width="100"
+                        height="100"
+                    />
                     <h5>{post.author?.username}</h5>
                 </div>
                 <div class="p-3">
-                    <div class="fa fa-calendar-alt"></div> {post.publication_date}
+                    <div class="fa fa-calendar-alt"></div>
+                    {post.publication_date}
                 </div>
+                <div class="sosmed p-2 text-center border-top">
+                    <a
+                        href="https://www.instagram.com/joe_arton?igsh=MTd6YzZkODY5Z3JndQ=="
+                        target="_blank"
+                    >
+                        <i class="bi bi-instagram"></i>
+                    </a>
+                    <a href="https://www.facebook.com/" target="_blank">
+                        <i class="bi bi-facebook"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/" target="_blank">
+                        <i class="bi bi-linkedin"></i>
+                    </a>
+                    <a href="https://www.twitter.com/" target="_blank">
+                        <i class="bi bi-twitter-x"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="other-post p-2">
+                <h6>Other Post</h6>
+
+                <SidePostMenu />
             </div>
         </div>
 
-        <div class="col-md-9">            
-            <article>{@html post.content}</article>
+        <!-- Info Content -->
+        <div class="col-md-9 shadow-sm mb-4 border-2">
+
+            
+
+            <article>{@html post.content}</article> <!-- Content -->
+
+            <div
+                class="apply w-100 shadow-sm bg-primary d-flex justify-content-between p-3 align-items-center mt-4"
+            >
+                <p class="text-white fw-semibold">Don't miss the chance!</p>
+                <button type="submit" class="btn btn-warning rounded-0 fw-bold text-primary"
+                    >Apply Now <i class="bi bi-arrow-right-circle-fill"></i>
+                </button>
+            </div>
+
         </div>
     </div>
 </div>
-  
 
 <style>
     .overlay {
@@ -65,7 +136,7 @@
         left: 0;
         width: 100%;
         height: 100%;
-        background: rgba(0, 0, 0, 0.1); 
+        background: rgba(0, 0, 0, 0.1);
         border-radius: 0.25rem;
     }
 
@@ -76,10 +147,28 @@
 
     .post-thumbnail-label {
         width: 70%;
-        bottom: 7%;        
+        bottom: 7%;
+        box-shadow: 1px 1px 8px rgb(255, 255, 255, 0.2);
     }
 
-    .post-title, .post-subtitle {
+    .post-title,
+    .post-subtitle {
         opacity: 0.95;
+    }
+
+    .sosmed a .bi {
+        color: #002e5c;
+        margin: 0.5rem;
+        transition: all 0.4s ease;
+    }
+
+    .sosmed a .bi:hover {
+        color: #ffcc00;
+    }
+
+    .info:hover {
+        transition: all 0.4s ease;
+        transform: scale(1.02);
+        box-shadow: 0 0 4px 2px rgba(0, 0, 0, 0.25);
     }
 </style>
